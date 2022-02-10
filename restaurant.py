fa5f39bc2cab26d0fbd7a52d893e0da680268a9c
Vincent Tuberion <72401317+vtuberion@users.noreply.github.com>
2022-02-09 19:05:21 -0500

    Add files via upload

--- restaurant.py
+++ restaurant.py
@@ -1,7 +1,7 @@
 # CS175L-50
 # Vincent Tuberion
 # restaurant.py
-# Last Modified 2/7/2022 22:39 EST
+# Last Modified 2/9/2022 19:04 EST
 
 vegetarian = ''
 vegan = ''
@@ -14,51 +14,68 @@ def boolConvert(stringVal):
     else:
         return False
 
+programRunning = True
+while programRunning:
+    print('----WELCOME TO THE RESTAURANT SELECTOR PROGRAM----')
+    dataValidated = False
+    while not dataValidated:
+        vegetarian = input('Is anyone in your party a vegetarian? (yes/no to answer)\n')
+        if vegetarian == 'yes' or vegetarian == 'no':
+            dataValidated = True
+        else:
+            print('UNKNOWN INPUT, PLEASE TRY AGAIN')
 
-print('----WELCOME TO THE RESTAURANT SELECTOR PROGRAM----')
-dataValidated = False
-while not dataValidated:
-    vegetarian = input('Is anyone in your party a vegetarian? (yes/no to answer)\n')
-    if vegetarian == 'yes' or vegetarian == 'no':
-        dataValidated = True
+    dataValidated = False
+    while not dataValidated:
+        vegan = input('Is anyone in your party a vegan? (yes/no to answer)\n')
+        if vegan == 'yes' or vegan == 'no':
+            dataValidated = True
+        else:
+            print('UNKNOWN INPUT, PLEASE TRY AGAIN')
+
+    dataValidated = False
+    while not dataValidated:
+        gluten_free = input('Is anyone in your party gluten free? (yes/no to answer)\n')
+        if gluten_free == 'yes' or gluten_free == 'no':
+            dataValidated = True
+        else:
+            print('UNKNOWN INPUT, PLEASE TRY AGAIN')
+
+    vegetarian = boolConvert(vegetarian)
+    vegan = boolConvert(vegan)
+    gluten_free = boolConvert(gluten_free)
+
+    print('''Here are all the restaurants on your list:
+       Joe’s Gourmet Burgers
+       Main Street Pizza Company
+       Corner Café
+       Mama’s Fine Italian
+       The Chef's Kitchen\n''')
+    print('Here are the restaurants you may eat at under the provided conditions:')
+    if not (vegetarian or vegan or gluten_free):
+        print('Joe’s Gourmet Burgers')
+    if not (vegan):
+        print('Main Street Pizza Company')
+    print('Corner Café')
+    if not (vegan or gluten_free):
+        print('Mama’s Fine Italian')
+    print('The Chef’s Kitchen')
+
+    askQuestion = ''
+    dataValidated = False
+    while not dataValidated:
+        askQuestion = input('Would you like to run a new search? (yes/no to answer)\n')
+        if askQuestion == 'yes' or askQuestion == 'no':
+            dataValidated = True
+        else:
+            print('UNKNOWN INPUT, PLEASE TRY AGAIN')
+    askQuestion = boolConvert(askQuestion)
+    if askQuestion:
+        print("Preparing a new search, please wait...")
     else:
-        print('UNKNOWN INPUT, PLEASE TRY AGAIN')
+        print("Thank you for choosing our program, have a nice day!")
+        programRunning = False
 
-dataValidated = False
-while not dataValidated:
-    vegan = input('Is anyone in your party a vegan? (yes/no to answer)\n')
-    if vegan == 'yes' or vegan == 'no':
-        dataValidated = True
-    else:
-        print('UNKNOWN INPUT, PLEASE TRY AGAIN')
-
-dataValidated = False
-while not dataValidated:
-    gluten_free = input('Is anyone in your party gluten free? (yes/no to answer)\n')
-    if gluten_free == 'yes' or gluten_free == 'no':
-        dataValidated = True
-    else:
-        print('UNKNOWN INPUT, PLEASE TRY AGAIN')
-
-vegetarian = boolConvert(vegetarian)
-vegan = boolConvert(vegan)
-gluten_free = boolConvert(gluten_free)
-
-print('''Here are all the restaurants on your list:
-   Joe’s Gourmet Burgers
-   Main Street Pizza Company
-   Corner Café
-   Mama’s Fine Italian
-   The Chef's Kitchen\n''')
-print('Here are the restaurants you may eat at under the provided conditions:')
-if not (vegetarian or vegan or gluten_free):
-    print('Joe’s Gourmet Burgers')
-if not (vegan):
-    print('Main Street Pizza Company')
-print('Corner Café')
-if not (vegan or gluten_free):
-    print('Mama’s Fine Italian')
-print('The Chef’s Kitchen')
 
 
 
