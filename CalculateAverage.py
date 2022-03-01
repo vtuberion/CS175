8a3f8c2973b4047b3be1ba394b7e45dd235fbddb
Vincent Tuberion <72401317+vtuberion@users.noreply.github.com>
2022-02-28 21:49:21 -0500

    Add files via upload

---
+++ CalculateAverage.py
@@ -0,0 +1,97 @@
+# CS175L-50
+# Vincent Tuberion
+# CalculateAverage.py
+# Last Modified 2/28/2022 21:47 EST
+
+# Generating placeholder variables to be used in the computations
+score1 = 0
+score2 = 0
+score3 = 0
+score4 = 0
+score5 = 0
+
+# ---------- FUNCTION CREATIONS ----------
+# Calculate the average of the 5 scores
+def calc_average():
+    average = (score1 + score2 + score3 + score4 + score5)/5
+    return average
+
+
+# Assign a letter grade based on a given number input
+def determine_grade(gradeNumber):
+    gradeLetter = ""  # Placeholder local variable creation to be set and then returned at the end
+    if gradeNumber >= 90:  # Anything above a 90 is an A letter grade
+        gradeLetter = "A"
+    elif 80 <= gradeNumber < 90:  # 80s turn into a B letter grade
+        gradeLetter = "B"
+    elif 70 <= gradeNumber < 80:  # 70s turn into a C letter grade
+        gradeLetter = "C"
+    elif 60 <= gradeNumber < 70:  # 60s turn into a D letter grade
+        gradeLetter = "D"
+    else:
+        gradeLetter = "F"  # Anything lower than a 60 will be considered failing, yielding an F letter grade
+    return gradeLetter  # Output the letter grade to wherever it needs to go
+
+
+# Assign valid input to a variable, keep harassing the user until they provide what they are asked to.
+def checkForInt(prompt):
+    validInput = False  # Local placeholder variable
+    while not validInput:  # Loop to keep asking the user until valid input is given
+        userInput = input(prompt)  # Take in new input
+        # My code in the past has had a history of being broken by Shrek/Bee Movie script, stop long inputs now
+        if len(userInput) > 10:
+            print("Okay, first of all, what kind of numbers are you putting in? Cut it out and do it right >:|")
+        else:
+            # If the input is not an excessive length, see if the string is actually a number input
+            if userInput.isnumeric():  # Check if the number is actually a number
+                userInput = float(userInput)  # Turn input into a float for decimal handling and formatting purposes
+                validInput = True  # Everything checks out, user may proceed to the next step of the program
+            else:
+                # Print an error message and make the user input data again
+                print("Invalid Input, please try again")
+    return userInput  # Assign the good user input to the variable that the function call is assigned to
+
+
+# Ask the user if they wish to repeat the program
+def repeat(prompt):
+    validInput = False  # Local placeholder variable
+    while not validInput:  # Loop to keep asking the user until valid input is given
+        userInput = input(prompt)  # Take in new input
+        # Same as checkForInt(), prevent excessive user input length
+        if len(userInput) > 10:
+            print("That is nowhere near what input you were asked to provide... do it again, CORRECTLY")
+        else:
+            userInput = userInput.lower()  # Turn input to lowercase to handle more input situations
+            if userInput == "yes":
+                print("Preparing another calculation for you, please stand by...")
+                validInput = True  # The input is valid, they said yes so we return true to run program again
+                return True
+            elif userInput == "no":
+                print("Ending Program, thank you for using our services!")
+                validInput = True  # The input is valid, they said no so we return false to end the program
+                return False
+            else:
+                # Input is not valid, make the user try again
+                print("Invalid Input, please try again. (Please use 'yes' or 'no')")
+
+# ---------- MAIN PROGRAM ----------
+running = True
+while running:  # Main loop to keep the program running, variable 'running' is the sentinel variable of the loop
+    # Take in only valid input for the 5 scores
+    score1 = checkForInt("Enter score 1: ")
+    score2 = checkForInt("Enter score 2: ")
+    score3 = checkForInt("Enter score 3: ")
+    score4 = checkForInt("Enter score 4: ")
+    score5 = checkForInt("Enter score 5: ")
+
+    # ---------- PROGRAM OUTPUT ----------
+    print(f'''Score		Numeric Grade	Letter Grade
+----------------------------------------------------
+score 1:	    {score1:.2f}		{determine_grade(score1)}
+score 2:	    {score2:.2f}		{determine_grade(score2)}
+score 3:	    {score3:.2f}		{determine_grade(score3)}
+score 4:	    {score4:.2f}		{determine_grade(score4)}
+score 5:	    {score5:.2f}	    {determine_grade(score5)}
+Average score:	{calc_average():.2f}	    {determine_grade(calc_average())}''')
+    # Run the function repeat() to check user input and see if user wants to run program again
+    running = repeat("Would you like to make another calculation? ('yes'/'no')\n")
