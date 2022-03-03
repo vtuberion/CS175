1a8682ecf3750f2e5b3dc6b30b35c1b8920804f6
Vincent Tuberion <72401317+vtuberion@users.noreply.github.com>
2022-03-02 20:11:56 -0500

    Add files via upload

--- CalculateAverage.py
+++ CalculateAverage.py
@@ -1,21 +1,29 @@
 # CS175L-50
 # Vincent Tuberion
 # CalculateAverage.py
-# Last Modified 2/28/2022 21:47 EST
+# Last Modified 2/28/2022 19:17 EST
+
+# ---------- IMPORT EXTERNALS ----------
+import my_random
 
 # Generating placeholder variables to be used in the computations
-score1 = 0
+# V1 variables
+'''score1 = 0
 score2 = 0
 score3 = 0
 score4 = 0
-score5 = 0
+score5 = 0'''
+# V2 variables
+scoreRange = 5
+scoreList = []
+scoreTotal = 0
+
 
 # ---------- FUNCTION CREATIONS ----------
 # Calculate the average of the 5 scores
-def calc_average():
-    average = (score1 + score2 + score3 + score4 + score5)/5
+def calc_average(total):
+    average = total/5
     return average
-
 
 # Assign a letter grade based on a given number input
 def determine_grade(gradeNumber):
@@ -33,8 +41,9 @@ def determine_grade(gradeNumber):
     return gradeLetter  # Output the letter grade to wherever it needs to go
 
 
+# V1 Only, number input not taken in V2
 # Assign valid input to a variable, keep harassing the user until they provide what they are asked to.
-def checkForInt(prompt):
+'''def checkForInt(prompt):
     validInput = False  # Local placeholder variable
     while not validInput:  # Loop to keep asking the user until valid input is given
         userInput = input(prompt)  # Take in new input
@@ -50,6 +59,7 @@ def checkForInt(prompt):
                 # Print an error message and make the user input data again
                 print("Invalid Input, please try again")
     return userInput  # Assign the good user input to the variable that the function call is assigned to
+'''
 
 
 # Ask the user if they wish to repeat the program
@@ -77,21 +87,27 @@ def repeat(prompt):
 # ---------- MAIN PROGRAM ----------
 running = True
 while running:  # Main loop to keep the program running, variable 'running' is the sentinel variable of the loop
+    # V1 Only, number input not taken in V2
     # Take in only valid input for the 5 scores
-    score1 = checkForInt("Enter score 1: ")
+    '''score1 = checkForInt("Enter score 1: ")
     score2 = checkForInt("Enter score 2: ")
     score3 = checkForInt("Enter score 3: ")
     score4 = checkForInt("Enter score 4: ")
-    score5 = checkForInt("Enter score 5: ")
-
+    score5 = checkForInt("Enter score 5: ")'''
+    # V2 features
+    # Clear total and scoreList in case user looped program (we don't want to double the list)
+    scoreList = []
+    scoreTotal = 0
+    for i in range(1, scoreRange+1):
+        scoreList.append(my_random.my_random(1, 100))
+        scoreTotal += scoreList[i-1]
+        print("New Auto-Generated Score:", scoreList[i-1])
     # ---------- PROGRAM OUTPUT ----------
     print(f'''Score		Numeric Grade	Letter Grade
-----------------------------------------------------
-score 1:	    {score1:.2f}		{determine_grade(score1)}
-score 2:	    {score2:.2f}		{determine_grade(score2)}
-score 3:	    {score3:.2f}		{determine_grade(score3)}
-score 4:	    {score4:.2f}		{determine_grade(score4)}
-score 5:	    {score5:.2f}	    {determine_grade(score5)}
-Average score:	{calc_average():.2f}	    {determine_grade(calc_average())}''')
+----------------------------------------------------''')
+    # V2 changed to list iteration print from multiline format print statement that did 5 scores individually
+    for i in range(1, scoreRange+1):
+        print(f"score {i}:	    {scoreList[i-1]:.2f}		{determine_grade(scoreList[i-1])}")
+    print(f"Average score:	{calc_average(scoreTotal):.2f}	    {determine_grade(calc_average(scoreTotal))}")
     # Run the function repeat() to check user input and see if user wants to run program again
     running = repeat("Would you like to make another calculation? ('yes'/'no')\n")
